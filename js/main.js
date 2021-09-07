3e4a61bf1158947874cd5e3ba72d0b5d6401ace6
NikBel3476 <nikita.belkin3476@gmail.com>
2021-09-07 22:12:51 +0400

    added libs

--- js/main.js
+++ js/main.js
@@ -1,71 +1,24 @@
-// Создание окна приложения
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 0x006400
-});
-document.body.appendChild(app.view);
+const canvas = new fabric.Canvas("scene");
 
-// Задний фон
-greenBackground = new PIXI.Graphics();
-greenBackground.beginFill(0x06c20c);
-greenBackground.drawRect(0, 0, app.screen.width - app.screen.width / 5, app.screen.height);
-greenBackground.position.set(app.screen.width / 10, 0);
-app.stage.addChild(greenBackground);
+const resize = () => {
+    canvas.setWidth(window.innerWidth);
+    canvas.setHeight(window.innerHeight);
+}
 
-// Должно отображать панельку с кнопками, но нееет!
-const buttonsPanel = PIXI.Sprite.from('src/sprites/CowsBackground.svg');
-buttonsPanel.anchor.set(0.5);
-buttonsPanel.x = 10;
-buttonsPanel.y = 10;
-app.stage.addChild(buttonsPanel);
+window.onresize = resize;
+window.onload = resize;
 
+
+
+// SvgJS ----------------------------------------------
 
 /*
-// Create window frame
-let frame = new PIXI.Graphics();
-frame.beginFill(0x666666);
-frame.lineStyle({ color: 0xffffff, width: 4, alignment: 0 });
-frame.drawRect(0, 0, 208, 208);
-frame.position.set(app.screen.heigth / 2, app.screen.width / 2);
-app.stage.addChild(frame);
+const svg = SVG().addTo('body').size(window.innerWidth, window.innerHeight);
 
-// Create a graphics object to define our mask
-let mask = new PIXI.Graphics();
-// Add the rectangular area to show
-mask.beginFill(0xffffff);
-mask.drawRect(0, 0, 200, 200);
-mask.endFill();
+window.onresize = () => {
+    svg.size(window.innerWidth, window.innerHeight);
+}
 
-// Add container that will hold our masked content
-let maskContainer = new PIXI.Container();
-// Set the mask to use our graphics object from above
-maskContainer.mask = mask;
-// Add the mask as a child, so that the mask is positioned relative to its parent
-maskContainer.addChild(mask);
-// Offset by the window's frame width
-maskContainer.position.set(4, 4);
-// And add the container to the window!
-frame.addChild(maskContainer);
+svg.image("src/sprites/Background.png");
 
-// Create contents for the masked container
-let text = new PIXI.Text(
-  'This text will scroll up and be masked, so you can see how masking works.  Lorem ipsum and all that.\n\n' +
-  'You can put anything in the container and it will be masked!',
-  {
-    fontSize: 24,
-    fill: 0x1010ff,
-    wordWrap: true,
-    wordWrapWidth: 180
-  }
-);
-text.x = 10;
-maskContainer.addChild(text);
-
-// Add a ticker callback to scroll the text up and down
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-  // Update the text's y coordinate to scroll it
-  elapsed += delta;
-  text.y = 10 + -100.0 + Math.cos(elapsed / 50.0) * 100.0;
-}); */
+svg.rect(100, 100).attr({ fill: "#ff0066"});*/
