fd4b8d2fea8b35cafcbb895bf62e23f7e57b184e
NikBel3476 <nikita.belkin3476@gmail.com>
2021-09-10 23:36:26 +0400

    added background

--- js/main.js
+++ js/main.js
@@ -1,12 +1,63 @@
 const canvas = new fabric.Canvas("scene");
 
+const imgPaths = {
+    background: "src/sprites/png/Background.png",
+};
+
+const IMGPaths = {
+    cowGrey: "src/sprites/png/CowGreyRight.png"
+}
+
+const objRes = {
+    fieldWidth: canvas.width / 20,
+    fieldHeight: canvas.height / 14
+}
+
+// On resize ---------------
+
 const resize = () => {
     canvas.setWidth(window.innerWidth);
     canvas.setHeight(window.innerHeight);
+    refreshObjRes();
+    canvas.clear();
+    setBackground(imgPaths.background);
+}
+
+function setBackground(imgPath = "") {
+    canvas.setBackgroundColor("#006400");
+    fabric.Image.fromURL(imgPath, (img) => {
+        img.scaleToWidth(canvas.width);
+        if (img.getScaledHeight() < canvas.height) {
+            canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
+        } else {
+            img.scaleToHeight(canvas.height);
+            canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+                left: (canvas.width - img.getScaledWidth()) / 2 // padding
+            });
+        }
+    });
+}
+
+// обновление размеров поля
+function refreshObjRes() {
+    objRes.fieldWidth = canvas.width / 20;
+    objRes.fieldHeight = canvas.height / 14;
+}
+
+function render() {
+    for (let imgPath in imgPaths) {
+        Object.values(IMGPaths).forEach((imgPath) => {
+            fabric.Image.fromURL(imgPath, (img) => {
+                img.scaleToWidth(100);
+                canvas.add(img);
+            });
+        });
+    }
 }
 
 window.onresize = resize;
-window.onload = resize;
-
-const cow = new Cow({x: 0, y: 0}, "right");
-console.log(cow);
+window.onload = () => {
+    resize();
+    setBackground(imgPaths.background);
+    render();
+};
