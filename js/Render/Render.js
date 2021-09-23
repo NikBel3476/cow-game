33452e15dce9223bad5ddcfece161b124a4ab132
NikBel3476 <nikita.belkin3476@gmail.com>
2021-09-23 20:31:46 +0400

    fix ui-table style

--- js/Render/Render.js
+++ js/Render/Render.js
@@ -9,17 +9,15 @@ class Render {
         this.gameTable = rows.map((row) => Array.from(row.getElementsByTagName("td")));
     }
 
-    drawScene(fields = {}, gameObjects = {}) {
-        console.log(this.gameTable);
-        // example
-        // rows[6].getElementsByTagName("td")[6].style.background = 'url("../../src/sprites/svg/CowGreyRight.svg") no-repeat center';
+    drawStaticObjects(fields = {}) {
         Object.keys(fields).forEach((fieldName) => {
-            // fill fields in the table
             fields[fieldName].forEach((coordinates) => {
-                console.log(`../../js/${CONF.ImgPath[fieldName]}`);
                 this.gameTable[coordinates[0] - 1][coordinates[1] - 1].style.background = `url("../../${CONF.ImgPath[fieldName]}") no-repeat center`;
             });
-            // gameTable[];
         });
+    }
+
+    drawScene(fields = {}, gameObjects = {}) {
+        this.drawStaticObjects(fields);
     }
 }
