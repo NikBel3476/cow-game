c779b3252d605919b227bffce95761164a7e53ab
NikBel3476 <nikita.belkin3476@gmail.com>
2023-08-06 21:27:56 +0400

    chore: add vite rescript plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import rescript from '@jihchi/vite-plugin-rescript';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), rescript()],
 	build: {
 		rollupOptions: {
 			input: {
